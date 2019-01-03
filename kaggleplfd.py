b00ed5a5e54a4ffc71ab148d7180e82e1b07743b
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2019-01-03 21:51:56 +0530

    Update kaggleplfd.py

--- kaggleplfd.py
+++ kaggleplfd.py
@@ -1,6 +1,8 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import os
-print(os.listdir("../input"))
-train = pd.read_parquet("../input/train.parquet")
-
+print(os.listdir("../input")) #just listing the files that I have
+train = pd.read_parquet("../input/train.parquet") #loading the training dataset.It is in apache parquet format
+import matplotlib.pyplot as plt #plotting some patterns.
+train.iloc[:, 0:3].plot(figsize=(12, 8))
+plt.axis('off');
