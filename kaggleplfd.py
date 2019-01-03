43c851e51acee983768760db02238bc6a4bab7bc
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2019-01-03 21:42:59 +0530

    Update kaggleplfd.py

--- kaggleplfd.py
+++ kaggleplfd.py
@@ -1,1 +1,6 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import os
+print(os.listdir("../input"))
+train = pd.read_parquet("../input/train.parquet")
 
