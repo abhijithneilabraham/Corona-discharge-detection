bb67ead191608cb703a9304811307fe8bb5c88d8
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2019-01-04 14:52:21 +0530

    Update sinwave.py

--- sinwave.py
+++ sinwave.py
@@ -15,13 +15,16 @@ and then try to match it together,by training using lstm.
 '''
 angle1=random()
 angle2=random()
-frequency1=60 #frequency range in range of 50-60 is chosen as standard.
-frequnecy2=50
+frequency1=100 #frequency range in range of 50-60 is chosen as standard.
+frequency2=200
 lag=23 #this defines how many steps ahead we are trying to predict
 def get_sample():
     #this function is gonna return a sin or cos value 
     global angle1,angle2
-    angle1+=2*pi/float(frequency1)
+    angle1+=2*pi/float(frequency1) #sampling the angles in the frequency period in
     angle2+=2*pi/float(frequency2)
+    angle1 %=2*pi
+    angle2 %=2*pi
     
+
 
