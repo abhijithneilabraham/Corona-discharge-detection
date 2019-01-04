72598ae3996da056a879a6b716051c4b8ab168b2
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2019-01-04 14:29:55 +0530

    created sinwav.py

---
+++ sinwave.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  4 14:28:05 2019
+
+@author: abhijithneilabraham
+"""
+
+from numpy import array,sin,cos,pi
+from random import random
