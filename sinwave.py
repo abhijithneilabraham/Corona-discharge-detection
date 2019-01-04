1f446840691107f6c9eb4af2a7bce1b3e1e96477
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2019-01-04 14:47:14 +0530

    Update sinwave.py

--- sinwave.py
+++ sinwave.py
@@ -8,9 +8,20 @@ Created on Fri Jan  4 14:28:05 2019
 
 from numpy import array,sin,cos,pi
 from random import random
-angle1=random() #choosing random initial values of angles
+'''
+choosing random initial values of angles 
+so,this value of angles is what we change according to the training dataset 
+and then try to match it together,by training using lstm.
+'''
+angle1=random()
 angle2=random()
 frequency1=60 #frequency range in range of 50-60 is chosen as standard.
 frequnecy2=50
 lag=23 #this defines how many steps ahead we are trying to predict
+def get_sample():
+    #this function is gonna return a sin or cos value 
+    global angle1,angle2
+    angle1+=2*pi/float(frequency1)
+    angle2+=2*pi/float(frequency2)
+    
 
